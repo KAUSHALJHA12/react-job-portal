96b27fbd584ec028b1f421db9c59464e3cbab739
Kaushal Jha <jhakaushal0017@gmail.com>
2026-01-04 06:33:07 +0530

    Fix auth middleware import case

--- backend/app.js
+++ backend/app.js
@@ -23,18 +23,16 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }));
 
-  app.options("*", (req, res) => {
+  app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://react-job-portal-omega.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,DELETE,OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-  return res.sendStatus(204);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
 });
 
 
